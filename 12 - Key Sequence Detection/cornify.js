e46b4f21ba7a186d4606f0280e3b58ecd1271a3c
bgiobbe <bgiobbe@gmail.com>
2023-08-08 13:44:14 -0400

    Add SameSite=Lax to cookie

--- 12 - Key Sequence Detection/cornify.js
+++ 12 - Key Sequence Detection/cornify.js
@@ -194,7 +194,8 @@ var cornify_setcookie = function (name, value, days) {
   var d = new Date();
   d.setTime(d.getTime() + days * 24 * 60 * 60 * 1000);
   var expires = "expires=" + d.toGMTString();
-  document.cookie = name + "=" + value + "; " + expires;
+  var sameSite = "; SameSite=Lax"
+  document.cookie = name + "=" + value + "; " + expires + sameSite;
 };
 
 var cornify_getcookie = function (cname) {
